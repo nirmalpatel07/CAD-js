34e8f61eaf231b37340c4d97083ce64be63a4037
nirmalpatel07 <138444412+nirmalpatel07@users.noreply.github.com>
2024-07-12 12:57:17 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use Strict"        //use all js code as newer version
+
+//alert(5+1)        //We are using nodeJs, not browser
+
+const accountId = "001"    // const values are not allowed to change
+let accountName = "Nirmal"    //Used to declare variables 
+var accountPass = "12345"      //old method to declare variables
+accountcity = "Rajkot"     //Not good way to declare variables
+let accountState           //undefined variable        no values assigned
+
+
+console.table([accountId , accountName , accountPass , accountcity, accountState]);
+
+
+/*DataTypes*/
+//number
+//bigint
+//string
+//boolean
+//null      Type of null is object      console.log(typeof null)
+//undefined
+//symbol => unique
+//object
+
