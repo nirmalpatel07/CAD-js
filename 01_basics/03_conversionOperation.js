6abda11688356998286158682208caba12289f26
nirmalpatel07 <138444412+nirmalpatel07@users.noreply.github.com>
2024-07-15 03:50:08 +0000

    datatype ssummary

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,14 @@
+let score = "33abc";
+
+console.log(typeof score);
+
+let number = Number(score);
+
+console.log(number);
+
+console.log(typeof number);
+
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1     false => 0
---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,11 @@
+//Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+
+
+
+
+// Reference (Non Primitive)
+
+// Array, Objects, Functions
