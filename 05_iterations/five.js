6121240f5d6c2320f8a5d67c260b305f9ad391e6
Nirmal Vaghasiya <138444412+nirmalpatel07@users.noreply.github.com>
2024-08-25 16:46:28 +0000

    foreach loop

---
+++ 05_iterations/five.js
@@ -0,0 +1,34 @@
+const coding = ["rb", "pt", "cpp","js"]
+
+// coding.forEach( function (item){
+//     console.log(item);
+    
+// } )
+
+// coding.forEach((item)=>{
+//     console.log(item);
+    
+// } )
+
+
+// function printme(item){
+//     console.log(item);
+// }
+// coding.forEach(printme)
+
+const myCoding = [
+    {
+        LName : "javascript",
+        FName : "js"
+    },
+    {
+        LName : "python",
+        FName : "py"
+    }
+]
+
+myCoding.forEach((item) => {
+    console.log(item.LName);
+    console.log(item.FName);
+    
+})
