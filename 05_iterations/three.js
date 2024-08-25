010947c57e81e8a5ef2d08118e5307a6e7ec2abe
Nirmal Vaghasiya <138444412+nirmalpatel07@users.noreply.github.com>
2024-08-25 13:40:19 +0000

    for of loop

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -4,4 +4,17 @@ map.set('EN', "England")
 map.set('RUS', "Russia")
 map.set('IN', "India")          //won't allow same value more than one time
 
-console.log(map);
+// console.log(map);
+
+
+//          Destructured     --      if you print only key it will print it as array
+//           |     |
+for (const [key, value] of map) {
+   // console.log(key, " : ",value);
+    
+}
+
+for (const key of map) {            // this method will print values as array
+    console.log(key);
+    
+}
