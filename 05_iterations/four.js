1517f19972156455f855d0b88fa0d81a0ee3cb47
Nirmal Vaghasiya <138444412+nirmalpatel07@users.noreply.github.com>
2024-08-25 16:06:27 +0000

    for in array

--- 05_iterations/four.js
+++ 05_iterations/four.js
@@ -1,12 +1,21 @@
-const myObject = {
-    js: 'javascript',
-    cpp: 'C++',
-    rb: 'ruby',
-}
+// const myObject = {
+//     js: 'javascript',
+//     cpp: 'C++',
+//     rb: 'ruby',
+// }                                               //Objects
 
-for (const key in myObject) {
-    // console.log(key);
-    console.log(`${key} for ${myObject[key]}`);
+// for (const key in myObject) {
+//     // console.log(key);
+//     console.log(`${key} for ${myObject[key]}`);
     
     
+// }
+
+//-----------------------------------
+
+const pro = ["rb", "py", "js" ,"cpp"]
+
+for (const key in pro) {                           //Array
+   console.log(pro[key]);
+   
 }
