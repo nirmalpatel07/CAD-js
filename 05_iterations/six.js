9ca4a9437005386eb5900be4efd780a860e1c937
Nirmal Vaghasiya <138444412+nirmalpatel07@users.noreply.github.com>
2024-08-27 13:34:13 +0000

    filter

---
+++ 05_iterations/six.js
@@ -0,0 +1,26 @@
+// const coding = ["rb", "pt", "cpp","js"]
+
+// const val = coding.forEach( function (item){        // not returns anything - undefined
+//     console.log(item);
+//     return item                         
+    
+// } )
+
+// console.log(val);
+
+
+ const myNums = [1,2,3,4,5,6,7,8,9,10]
+// const newNums = myNums.filter((i) =>i>4)
+// console.log(newNums);
+
+
+// const newNums = []
+
+// myNums.forEach((num) => {
+//     if(num > 4){
+//         newNums.push(num)
+//     }
+// });
+// console.log(newNums);
+
+
